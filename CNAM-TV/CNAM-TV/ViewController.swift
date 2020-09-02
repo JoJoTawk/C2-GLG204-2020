a0b87dcec2b0d298b091c1a4fde544632d97a0af
Jhonny <Jhonny.tawk@mcloudagency.com>
2020-09-03 01:30:46 +0300

    Initializing iOS Project with Scene iOS 13

---
+++ CNAM-TV/CNAM-TV/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CNAM-TV
+//
+//  Created by Jhonny on 9/3/20.
+//  Copyright © 2020 Baloomba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
